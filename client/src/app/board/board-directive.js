2cb6fea506d514077fd3cc0b13ac7b9c704efa4c
atorma <anssi.torma@iki.fi>
2015-10-21 07:07:19 +0300

    Board directive: linking function encapsulates instance-specific variables and functions

--- client/src/app/board/board-directive.js
+++ client/src/app/board/board-directive.js
@@ -18,139 +18,145 @@ function board() {
     }
 }
 
-var canvas, ctx;
 
-var numRows, numCols;
-var cellWidth, cellHeight;
-
-var gridColor = "#eee";
-var pieceColor = "#000";
-
-var currentMethod = drawCross;
-
+var GRID_COLOR = "#eee";
+var PIECE_COLOR = "#000";
 
 function link($scope, iElem, iAttrs) {
-    canvas = iElem.find("canvas")[0];
-    ctx = canvas.getContext("2d");
+    var canvas = iElem.find("canvas")[0];
+    var ctx = canvas.getContext("2d");
 
     var size = _.floor(new Number(iAttrs.size));
-    numRows = size;
-    numCols = size;
+    var numRows = size;
+    var numCols = size;
 
     canvas.width = 400;
     canvas.height = 400;
 
-    cellWidth = canvas.width/numCols;
-    cellHeight = canvas.height/numRows;
+    var cellWidth = canvas.width/numCols;
+    var cellHeight = canvas.height/numRows;
+
+    var currentMethod = drawCross;
+
+
+    canvas.onclick = onCanvasClick;
+
 
     drawGameBoard();
 
-    canvas.onclick = onCanvasClick;
+
+    //---------------------------------------------------//
+
+    function drawGameBoard() {
+        ctx.strokeStyle = GRID_COLOR;
+
+        // Draw horizontal lines
+        for (var i = 0; i < numRows - 1; i++) {
+            ctx.beginPath();
+            ctx.moveTo(0, cellHeight*(i + 1));
+            ctx.lineTo(canvas.width, cellHeight*(i + 1));
+            ctx.stroke();
+        }
+
+        // Draw vertical lines
+        for (var i = 0; i < numCols - 1; i++) {
+            ctx.beginPath();
+            ctx.moveTo(cellWidth*(i + 1), 0);
+            ctx.lineTo(cellWidth*(i + 1), canvas.height);
+            ctx.stroke();
+        }
+
+    }
+
+
+    function drawCross(cell) {
+        ctx.strokeStyle = PIECE_COLOR;
+
+        ctx.beginPath();
+        ctx.moveTo(cell.column*cellWidth, cell.row*cellHeight);
+        ctx.lineTo((cell.column + 1)*cellWidth, (cell.row + 1)*cellHeight);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo((cell.column + 1)*cellWidth, cell.row*cellHeight);
+        ctx.lineTo(cell.column*cellWidth, (cell.row + 1)*cellHeight);
+        ctx.stroke();
+    }
+
+
+    function drawCircle(cell) {
+        ctx.strokeStyle = PIECE_COLOR;
+
+        ctx.beginPath();
+        ctx.arc((cell.column + 1/2)*cellWidth, (cell.row + 1/2)*cellWidth, cellWidth/2, 0, 2*Math.PI);
+        ctx.stroke();
+    }
+
+
+    function drawLine(start, end) {
+
+        var startX, startY, endX, endY;
+
+        if (start.row == end.row) { // horizontal
+            startX = start.column*cellWidth;
+            startY = (start.row + 1/2)*cellHeight;
+            endX = (end.column + 1)*cellWidth;
+            endY = startY;
+        } else if (start.column == end.column) { // vertical
+            startX = (start.column + 1/2)*cellWidth;
+            startY = start.row*cellHeight;
+            endX = startX;
+            endY = (end.row + 1)*cellHeight;
+        } else if (start.row < end.row && start.column < end.column) { // diagonal top-left to bottom-right
+            startX = start.column*cellWidth;
+            startY = start.row*cellHeight;
+            endX = (end.column + 1)*cellWidth;
+            endY = (end.row + 1)*cellHeight;
+        } else if (start.row < end.row && start.column > end.column) { // diagonal top-right to bottom-left
+            startX = (start.column + 1)*cellWidth;
+            startY = start.row*cellHeight;
+            endX = end.column*cellWidth;
+            endY = (end.row + 1)*cellHeight;
+        } else {
+            var template = _.template("Invalid line (<%= startRow %>, <%= startCol %>) to (<%= endRow %>, <%= endCol%>)");
+            throw template({startRow: start.row, startCol: start.column, endRow: end.row, endCol: end.column});
+        }
+
+        ctx.strokeStyle = PIECE_COLOR;
+        ctx.beginPath();
+        ctx.moveTo(startX, startY);
+        ctx.lineTo(endX, endY);
+        ctx.stroke();
+    }
+
+    function onCanvasClick(e) {
+        var cc = getCanvasCoordinates(e);
+        var cell = getCellCoordinates(cc);
+
+        // TODO temporary, for testing
+        currentMethod(cell);
+        if (currentMethod == drawCross) {
+            currentMethod = drawCircle;
+        } else {
+            currentMethod = drawCross;
+        }
+    }
+
+    function getCanvasCoordinates(clickEvent) {
+        return {
+            x: clickEvent.pageX - canvas.offsetLeft,
+            y: clickEvent.pageY - canvas.offsetTop
+        }
+    }
+
+    function getCellCoordinates(canvasCoordinates) {
+        return {
+            row: _.floor(canvasCoordinates.y/cellHeight),
+            column: _.floor(canvasCoordinates.x/cellWidth)
+        }
+    }
 };
 
 
-function drawGameBoard() {
-    ctx.strokeStyle = gridColor;
 
-    // Draw horizontal lines
-    for (var i = 0; i < numRows - 1; i++) {
-        ctx.beginPath();
-        ctx.moveTo(0, cellHeight*(i + 1));
-        ctx.lineTo(canvas.width, cellHeight*(i + 1));
-        ctx.stroke();
-    }
 
-    // Draw vertical lines
-    for (var i = 0; i < numCols - 1; i++) {
-        ctx.beginPath();
-        ctx.moveTo(cellWidth*(i + 1), 0);
-        ctx.lineTo(cellWidth*(i + 1), canvas.height);
-        ctx.stroke();
-    }
-
-}
-
-function drawCross(cell) {
-    ctx.strokeStyle = pieceColor;
-
-    ctx.beginPath();
-    ctx.moveTo(cell.column*cellWidth, cell.row*cellHeight);
-    ctx.lineTo((cell.column + 1)*cellWidth, (cell.row + 1)*cellHeight);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo((cell.column + 1)*cellWidth, cell.row*cellHeight);
-    ctx.lineTo(cell.column*cellWidth, (cell.row + 1)*cellHeight);
-    ctx.stroke();
-}
-
-function drawCircle(cell) {
-    ctx.strokeStyle = pieceColor;
-
-    ctx.beginPath();
-    ctx.arc((cell.column + 1/2)*cellWidth, (cell.row + 1/2)*cellWidth, cellWidth/2, 0, 2*Math.PI);
-    ctx.stroke();
-}
-
-function drawLine(start, end) {
-    ctx.strokeStyle = pieceColor;
-
-    var startX, startY, endX, endY;
-
-    if (start.row == end.row) { // horizontal
-        startX = start.column*cellWidth;
-        startY = (start.row + 1/2)*cellHeight;
-        endX = (end.column + 1)*cellWidth;
-        endY = startY;
-    } else if (start.column == end.column) { // vertical
-        startX = (start.column + 1/2)*cellWidth;
-        startY = start.row*cellHeight;
-        endX = startX;
-        endY = (end.row + 1)*cellHeight;
-    } else if (start.row < end.row && start.column < end.column) { // diagonal top-left to bottom-right
-        startX = start.column*cellWidth;
-        startY = start.row*cellHeight;
-        endX = (end.column + 1)*cellWidth;
-        endY = (end.row + 1)*cellHeight;
-    } else if (start.row < end.row && start.column > end.column) { // diagonal top-right to bottom-left
-        startX = (start.column + 1)*cellWidth;
-        startY = start.row*cellHeight;
-        endX = end.column*cellWidth;
-        endY = (end.row + 1)*cellHeight;
-    } else {
-        var template = _.template("Invalid line (<%= startRow %>, <%= startCol %>) to (<%= endRow %>, <%= endCol%>)");
-        throw template({startRow: start.row, startCol: start.column, endRow: end.row, endCol: end.column});
-    }
-
-    ctx.beginPath();
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(endX, endY);
-    ctx.stroke();
-}
-
-function onCanvasClick(e) {
-    var cc = getCanvasCoordinates(e);
-    var cell = getCellCoordinates(cc);
-
-    // TODO temporary, for testing
-    currentMethod(cell);
-    if (currentMethod == drawCross) {
-        currentMethod = drawCircle;
-    } else {
-        currentMethod = drawCross;
-    }
-}
-
-function getCanvasCoordinates(clickEvent) {
-    return {
-        x: clickEvent.pageX - canvas.offsetLeft,
-        y: clickEvent.pageY - canvas.offsetTop
-    }
-}
-
-function getCellCoordinates(canvasCoordinates) {
-    return {
-        row: _.floor(canvasCoordinates.y/cellHeight),
-        column: _.floor(canvasCoordinates.x/cellWidth)
-    }
-}
