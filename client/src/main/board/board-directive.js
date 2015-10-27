8100d848cac70a7e83230ae71a9f152c4bf1131d
atorma <anssi.torma@iki.fi>
2015-10-27 07:59:05 +0200

    Fixed: board draws circle when cross moved and vice versa

--- client/src/main/board/board-directive.js
+++ client/src/main/board/board-directive.js
@@ -74,9 +74,9 @@ function board(GAME_EVENTS, PIECES) {
 
         function onMoveCompleted(event, result) {
             if (result.move.piece === PIECES.O) {
-                drawCross(result.move.cell);
-            } else if (result.move.piece === PIECES.X) {
                 drawCircle(result.move.cell);
+            } else if (result.move.piece === PIECES.X) {
+                drawCross(result.move.cell);
             }
 
             if (result.winningSequence) {
