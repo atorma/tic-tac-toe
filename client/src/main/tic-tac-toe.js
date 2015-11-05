dbc23e9ef526aaf4a21768c338ba54a17a46cdf9
atorma <anssi.torma@iki.fi>
2015-11-05 07:46:45 +0200

    Added icons play, pause, stop, forward

--- client/src/main/tic-tac-toe.js
+++ client/src/main/tic-tac-toe.js
@@ -20,11 +20,23 @@ angular
     .module("ticTacToe", ["ngMaterial", "ui.router"])
     .constant("GAME_EVENTS", GAME_EVENTS)
     .constant("PIECES", PIECES)
+    .config(configureIcons)
     .run(makeStateAvailableInScope);
 
 require("./route-config");
 require("./board");
 require("./game");
+
+
+
+
+function configureIcons($mdIconProvider) {
+    $mdIconProvider
+        .icon("play", "resources/material-design-icons/ic_play_arrow_black_24px.svg")
+        .icon("pause", "resources/material-design-icons/ic_pause_black_24px.svg")
+        .icon("stop", "resources/material-design-icons/ic_stop_black_24px.svg")
+        .icon("forward", "resources/material-design-icons/ic_forward_black_24px.svg");
+}
 
 // Injection of $state may trigger a GET, which can show as an 
 // "Unexpected request" error in your test. Workaround is to 
