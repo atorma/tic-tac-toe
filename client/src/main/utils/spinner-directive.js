f81b842e352f50ae9b84c153b37cd2e347fb9ba3
atorma <anssi.torma@iki.fi>
2015-11-22 22:19:57 +0200

    * Spinner directive size parameter: set size of spinner box in px * Separated class centered from other spinner css

--- client/src/main/utils/spinner-directive.js
+++ client/src/main/utils/spinner-directive.js
@@ -1,23 +1,32 @@
 "use strict";
 
 var angular = require("angular");
+var _ = require("lodash");
 
 angular.module("ticTacToe")
     .directive("spinner", spinner);
 
 
-var TEMPLATE = [
-    '<div class="loader">',
-        '<svg class="circular" viewBox="0 0 24 24">',
-            '<circle class="path" cx="12" cy="12" r="10" fill="none" stroke-width="2" stroke-miterlimit="10"/>',
+var templateFn = _.template([
+    '<div class="loader" style="width: <%= size %>px">',
+        '<svg class="circular" viewBox="0 0 <%= size %> <%= size %>">',
+            '<circle class="path" cx="<%= center %>" cy="<%= center %>" r="<%= radius %>" fill="none" stroke-width="<%= width %>" stroke-miterlimit="10"/>',
         '</svg>',
     '</div>',
-].join('');
+].join(''));
 
 function spinner() {
     return {
         restrict: "E",
-        template: TEMPLATE
+        template: template
     };
+
+    function template(tElem, tAttrs) {
+        var size = tAttrs.size ? parseInt(tAttrs.size) : 24;
+        var center = size/2;
+        var radius = size/2 - 2;
+        var width = _.floor(radius/5);
+        return templateFn({size: size, center: center, radius: radius, width: width});
+    }
 }
 
