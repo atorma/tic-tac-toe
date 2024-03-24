86f16b7280af4c85168d08f6a4cb79679e686362
Anssi Törmä <anssi.torma@iki.fi>
2024-03-24 20:20:59 +0200

    Add deploy script using Kustomization

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+set -e
+
+cd "$(dirname "$0")"
+
+. utils.sh
+. .env-build
+
+cd k8s
+
+kustomize edit set image tic-tac-toe-image="$GCLOUD_REPOSITORY/tic-tac-toe-server:$(get_commit_hash)"
+kubectl apply -k .
+git restore kustomization.yaml
