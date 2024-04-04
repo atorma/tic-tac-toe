a734b291c9f4f417205087a93ea34a06bdc78794
Anssi Törmä <anssi.torma@iki.fi>
2024-04-04 19:53:04 +0300

    Add another test

--- src/test/java/org/atorma/tictactoe/game/player/mcts/AiPlayTests.java
+++ src/test/java/org/atorma/tictactoe/game/player/mcts/AiPlayTests.java
@@ -34,7 +34,7 @@ public class AiPlayTests {
         winsMap.put(Piece.O, 0);
 
         int nRounds = 50;
-        for (int round = 0; round < nRounds; round++) {
+        for (int round = 1; round <= nRounds; round++) {
             LOGGER.info("Starting round {}", round);
             MCTSPlayer startingPlayer = Math.random() < 0.5 ? naiveMctsPlayer : uniformRandomMctsPlayer;
             GameState startingState = GameState.builder().setConnectHowMany(5)
@@ -55,6 +55,50 @@ public class AiPlayTests {
                 winsMap.get(naiveMctsPlayer.getPiece()),
                 winsMap.get(uniformRandomMctsPlayer.getPiece()),
                 nRounds - winsMap.get(naiveMctsPlayer.getPiece()) - winsMap.get(uniformRandomMctsPlayer.getPiece()));
+    }
+
+    @Test
+    public void mcts_nearby_cells_vs_mcts_uniform_random() {
+        MCTSParameters randomNearby = new MCTSParameters();
+        randomNearby.simulationStrategy = MCTSParameters.SimulationStrategy.RANDOM_ADJACENT;
+        randomNearby.searchRadius = 1;
+        randomNearby.gamesPerRollout = 1;
+        MCTSPlayer randomNearbyMctsPlayer = new MCTSPlayer(randomNearby);
+        randomNearbyMctsPlayer.setPiece(Piece.X);
+
+        MCTSParameters uniformRandomParams = new MCTSParameters();
+        uniformRandomParams.simulationStrategy = MCTSParameters.SimulationStrategy.UNIFORM_RANDOM;
+        uniformRandomParams.searchRadius = 2;
+        uniformRandomParams.gamesPerRollout = 1;
+        MCTSPlayer uniformRandomMctsPlayer = new MCTSPlayer(uniformRandomParams);
+        uniformRandomMctsPlayer.setPiece(Piece.O);
+
+        Map<Piece, Integer> winsMap = new EnumMap<>(Piece.class);
+        winsMap.put(Piece.X, 0);
+        winsMap.put(Piece.O, 0);
+
+        int nRounds = 10;
+        for (int round = 1; round <= nRounds; round++) {
+            LOGGER.info("Starting round {}", round);
+            MCTSPlayer startingPlayer = Math.random() < 0.5 ? randomNearbyMctsPlayer : uniformRandomMctsPlayer;
+            GameState startingState = GameState.builder().setConnectHowMany(5)
+                    .setBoard(new Piece[18][18])
+                    .setNextPlayer(startingPlayer.getPiece())
+                    .build();
+            Simulator simulator = new Simulator(startingState, randomNearbyMctsPlayer, uniformRandomMctsPlayer);
+            GameState endState = simulator.run();
+            Piece winner = endState.getWinner();
+            Integer wins = winsMap.get(winner);
+            int updatedWins = wins + 1;
+            winsMap.put(winner, updatedWins);
+            LOGGER.info("Winner of round {}: {}, now has {} wins", round, winner, updatedWins);
+        }
+
+
+        LOGGER.info("MCTS Naive heuristics: {} wins, MCTS uniform random: {} wins, draws: {}",
+                winsMap.get(randomNearbyMctsPlayer.getPiece()),
+                winsMap.get(uniformRandomMctsPlayer.getPiece()),
+                nRounds - winsMap.get(randomNearbyMctsPlayer.getPiece()) - winsMap.get(uniformRandomMctsPlayer.getPiece()));
     }
 
 }
