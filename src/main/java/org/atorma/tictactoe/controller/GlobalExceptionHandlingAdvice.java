5859a7a0b424375f0b1db5ccdc9317ce7d2ae5e5
Anssi Törmä <anssi.torma@iki.fi>
2024-04-06 12:55:52 +0300

    Add global exception handler for MethodArgumentNotValidException.class, fix failing test

--- src/main/java/org/atorma/tictactoe/controller/GlobalExceptionHandlingAdvice.java
+++ src/main/java/org/atorma/tictactoe/controller/GlobalExceptionHandlingAdvice.java
@@ -2,10 +2,18 @@ package org.atorma.tictactoe.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandlingAdvice {
@@ -16,6 +24,19 @@ public class GlobalExceptionHandlingAdvice {
     public ResponseEntity<Void> logAndReturnInternalServerError(Exception e) {
         LOGGER.error("Unhandled exception", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getFieldErrors()
+                .stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
     }
 
 }
