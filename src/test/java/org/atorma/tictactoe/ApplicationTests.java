68fc5ed4462df67b9402c12c759db82d71197e7e
Anssi Törmä <anssi.torma@iki.fi>
2024-04-06 14:33:50 +0300

    Update to Spring Boot 1.5.22

--- src/test/java/org/atorma/tictactoe/ApplicationTests.java
+++ src/test/java/org/atorma/tictactoe/ApplicationTests.java
@@ -1,13 +1,13 @@
 package org.atorma.tictactoe;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = TicTacToeApplication.class)
+@SpringBootTest(classes = TicTacToeApplication.class)
 @WebAppConfiguration
 public abstract class ApplicationTests {
 
