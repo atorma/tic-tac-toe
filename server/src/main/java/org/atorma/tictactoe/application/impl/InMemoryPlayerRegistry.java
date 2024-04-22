85556ae54f76fd81a3b82ab2f3ae10cfaccd6786
Anssi Törmä <anssi.torma@iki.fi>
2024-04-22 17:22:22 +0300

    Decrease default games per rollout for MCTS Random Nearby

--- server/src/main/java/org/atorma/tictactoe/application/impl/InMemoryPlayerRegistry.java
+++ server/src/main/java/org/atorma/tictactoe/application/impl/InMemoryPlayerRegistry.java
@@ -40,7 +40,7 @@ public class InMemoryPlayerRegistry implements PlayerRegistry {
 
         MCTSParameters randomNearbyParams = new MCTSParameters();
         randomNearbyParams.simulationStrategy = MCTSParameters.SimulationStrategy.RANDOM_NEARBY;
-        randomNearbyParams.gamesPerRollout = parseEnvVarAsInt("MCTS_RANDOM_NEARBY_GAMES_PER_ROLLOUT", 200);
+        randomNearbyParams.gamesPerRollout = parseEnvVarAsInt("MCTS_RANDOM_NEARBY_GAMES_PER_ROLLOUT", 25);
         randomNearbyParams.searchRadius = 2;
         randomNearbyParams.pruneDescendantLevelsGreaterThan = parseEnvVarAsInt("MCTS_MAX_DESCENDANT_LEVEL", Integer.MAX_VALUE);
         addPlayerInfo("MCTS random nearby", PlayerInfo.Type.AI, MCTSPlayer.class, randomNearbyParams);
